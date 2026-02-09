57f3881725ffd2bad7066c39aee798d26b706560
Luke Marchant <gswebmaster@byu.edu>
2026-02-09 14:21:38 -0700

    Added some comments

--- Mission6_Marchant/Models/MoviesContext.cs
+++ Mission6_Marchant/Models/MoviesContext.cs
@@ -1,7 +1,7 @@
 using Microsoft.EntityFrameworkCore;
 
 namespace Mission6.Models;
-
+//Context file to help connect the database
 public class MoviesContext : DbContext
 {
     public MoviesContext(DbContextOptions<MoviesContext> options) : base (options)
